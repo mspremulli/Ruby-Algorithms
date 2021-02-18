d23b343355053acdc888a9440d319e5b4ac6c8a3
Michael Spremulli <michaels.wpi@gmail.com>
2021-02-18 11:30:40 -0500

    added slice! solution

--- algorithms/removeArrayPart.rb
+++ algorithms/removeArrayPart.rb
@@ -9,4 +9,11 @@ def removeArrayPart(inputArray, l, r)
     return newArr
 end
 
+def removeArrayPart2(inputArray, l, r)
+    inputArray.slice!(l, r-l + 1)
+    return inputArray
+end
+
 puts(removeArrayPart([1,2,3,4,5], 2, 3))
+puts
+puts(removeArrayPart2([1,2,3,4,5], 2, 3))
